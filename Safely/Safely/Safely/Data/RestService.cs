efb1d2c439262637ae5e21b6f300fa865495b6f4
FynnSu <fynnsu@outlook.com>
2020-03-28 11:45:57 -0400

    Add FirebaseDatabase NuGet to project

---
+++ Safely/Safely/Safely/Data/RestService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Safely.Data
+{
+    class RestService
+    {
+        HttpClient client;
+    }
+}
