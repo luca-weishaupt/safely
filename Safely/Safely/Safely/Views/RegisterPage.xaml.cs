4c794e9f68cc6d928455525a546de0b7128c0902
FynnSu <fynnsu@outlook.com>
2020-03-28 17:42:18 -0400

    Register page backend

--- Safely/Safely/Safely/Views/RegisterPage.xaml.cs
+++ Safely/Safely/Safely/Views/RegisterPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Safely.Data;
+using Safely.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,7 @@ namespace Safely
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RegisterPage : ContentPage
     {
+        FirebaseHelper firebaseHelper = new FirebaseHelper();
         public RegisterPage()
         {
             InitializeComponent();
@@ -19,12 +22,19 @@ namespace Safely
 
          async void RegisterProcedure(object sender, EventArgs e)
         {
-            if (RegisterEmail.Text.Equals("") || RegisterPassword.Equals(""))
+            if (RegisterEmail.Text.Equals("") || RegisterPassword.Text.Equals(""))
             {
-                await DisplayAlert("Login", "Login Success", "Ok");
+                await DisplayAlert("Register", "Register Failed, You must enter both an email and a password", "Ok");
                 return;
             }
-
+            string email = RegisterEmail.Text;
+            var existingUser = firebaseHelper.GetUser(email);
+            if (existingUser != null)
+            {
+                await DisplayAlert("Register", "Register failed, a user with the same email address already exists", "Ok");
+                return;
+            }
+            await firebaseHelper.AddUser(email, RegisterPassword.Text);
         }
     }
 }
